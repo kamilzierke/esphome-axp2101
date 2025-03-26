31fdd7bbfe9d2605018636c19e23b289e7b0ad14
kamilzierke <67487992+kamilzierke@users.noreply.github.com>
2025-03-26 02:46:47 +0100

    Update axp2101.h

--- components/axp2101/axp2101.h
+++ components/axp2101/axp2101.h
@@ -4,7 +4,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
-
+#include "esphome/components/binary_sensor/binary_sensor.h"
 namespace esphome {
 namespace axp2101 {
 
