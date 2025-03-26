7d2ce7bf0c236657f21f3b04e14becc22b982c14
kamilzierke <67487992+kamilzierke@users.noreply.github.com>
2025-03-26 01:09:54 +0100

    Update sensor.py

--- components/axp2101/sensor.py
+++ components/axp2101/sensor.py
@@ -1,85 +1,107 @@
+
+from esphome import automation
+from esphome.components import i2c, sensor, binary_sensor
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor, i2c, sensor
 from esphome.const import (
-    CONF_BATTERY_LEVEL,
-    CONF_BATTERY_VOLTAGE,
-    CONF_BRIGHTNESS,
     CONF_ID,
-    CONF_MODEL,
-    DEVICE_CLASS_BATTERY_CHARGING,
+    CONF_ADDRESS,
+    CONF_UPDATE_INTERVAL,
     DEVICE_CLASS_VOLTAGE,
-    ENTITY_CATEGORY_DIAGNOSTIC,
-    ICON_BATTERY,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_TEMPERATURE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_VOLT,
+    UNIT_AMPERE,
+    UNIT_CELSIUS,
     UNIT_PERCENT,
+    UNIT_WATT,
 )
 
 DEPENDENCIES = ["i2c"]
 
 axp2101_ns = cg.esphome_ns.namespace("axp2101")
-AXP2101Component = axp2101_ns.class_(
-    "AXP2101Component", cg.PollingComponent, i2c.I2CDevice
-)
-AXP2101Model = axp2101_ns.enum("AXP2101Model")
+AXP2101Component = axp2101_ns.class_("AXP2101Component", cg.PollingComponent, i2c.I2CDevice)
 
-MODELS = {
-    "M5CORE2": AXP2101Model.AXP2101_M5CORE2,
-}
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(AXP2101Component),
+    cv.Optional(CONF_ADDRESS, default=0x34): cv.i2c_address,
+    cv.Optional(CONF_UPDATE_INTERVAL): cv.update_interval,
+    cv.Optional("battery_voltage"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("battery_current"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_CURRENT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("battery_power"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_POWER,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("battery_level"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_PERCENT,
+        accuracy_decimals=1,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("dc_in_voltage"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("dc_in_current"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=2,
+        device_class=DEVICE_CLASS_CURRENT,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("temperature"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional("charging"): binary_sensor.binary_sensor_schema(),
+    cv.Optional("acin_connected"): binary_sensor.binary_sensor_schema(),
+}).extend(cv.polling_component_schema("60s")).extend(i2c.i2c_device_schema())
 
-AXP2101_MODEL = cv.enum(MODELS, upper=True, space="_")
-
-CONF_BATTERY_CHARGING = "battery_charging"
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(AXP2101Component),
-            cv.Required(CONF_MODEL): AXP2101_MODEL,
-            cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(
-                device_class=DEVICE_CLASS_VOLTAGE,
-                accuracy_decimals=3,
-                entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-            ),
-            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
-                unit_of_measurement=UNIT_PERCENT,
-                accuracy_decimals=0,
-                icon=ICON_BATTERY,
-            ),
-            cv.Optional(CONF_BATTERY_CHARGING): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_BATTERY_CHARGING,
-            ),
-            cv.Optional(CONF_BRIGHTNESS, default=1.0): cv.percentage,
-        }
-    )
-    .extend(cv.polling_component_schema("60s"))
-    .extend(i2c.i2c_device_schema(0x77))
-)
-
-
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield i2c.register_i2c_device(var, config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
 
-    cg.add_library("lewisxhe/XPowersLib", "0.2.1")
-
-    cg.add(var.set_model(config[CONF_MODEL]))
-
-    if CONF_BATTERY_VOLTAGE in config:
-        conf = config[CONF_BATTERY_VOLTAGE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_batteryvoltage_sensor(sens))
-
-    if CONF_BATTERY_LEVEL in config:
-        conf = config[CONF_BATTERY_LEVEL]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_batterylevel_sensor(sens))
-
-    if CONF_BATTERY_CHARGING in config:
-        conf = config[CONF_BATTERY_CHARGING]
-        sens = yield binary_sensor.new_binary_sensor(conf)
-        cg.add(var.set_batterycharging_bsensor(sens))
-
-    if CONF_BRIGHTNESS in config:
-        conf = config[CONF_BRIGHTNESS]
-        cg.add(var.set_brightness(conf))
+    if conf := config.get("battery_voltage"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_battery_voltage_sensor(sens))
+    if conf := config.get("battery_current"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_battery_current_sensor(sens))
+    if conf := config.get("battery_power"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_battery_power_sensor(sens))
+    if conf := config.get("battery_level"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_battery_level_sensor(sens))
+    if conf := config.get("dc_in_voltage"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_dc_in_voltage_sensor(sens))
+    if conf := config.get("dc_in_current"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_dc_in_current_sensor(sens))
+    if conf := config.get("temperature"):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_temperature_sensor(sens))
+    if conf := config.get("charging"):
+        bin_sens = await binary_sensor.new_binary_sensor(conf)
+        cg.add(var.set_charging_sensor(bin_sens))
+    if conf := config.get("acin_connected"):
+        bin_sens = await binary_sensor.new_binary_sensor(conf)
+        cg.add(var.set_acin_connected_sensor(bin_sens))
